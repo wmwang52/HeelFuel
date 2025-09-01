f705fe3e7c85406056b347aecad9e207043dcd7e
William Wang <wmwang52@gmail.com>
2025-09-01 16:35:10 -0400

    Starting Project from Fresh

---
+++ HeelFuel/HeelFuel/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HeelFuel
+//
+//  Created by William Wang on 9/1/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ HeelFuel/HeelFuelTests/HeelFuelTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeelFuelTests.swift
+//  HeelFuelTests
+//
+//  Created by William Wang on 9/1/25.
+//
+
+import Testing
+@testable import HeelFuel
+
+struct HeelFuelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
